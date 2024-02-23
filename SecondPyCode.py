9b0f681688014104de87714789fca6902bf57ff7
George Ayman <george.ayman10@hotmail.com>
2024-02-23 23:29:59 +0200

    Lists,Tuples,Sets

--- SecondPyCode.py
+++ SecondPyCode.py
@@ -1,1 +1,8 @@
-
+my_list = ['Hi' ,'Hi' , 'I am' , 'Pro', 'Python','Programmer']
+print(my_list)
+my_tuple = (my_list)
+print(my_tuple)
+my_set = set(my_tuple)
+print(my_set)
+my_set.add('Number 1')
+print(my_set)
